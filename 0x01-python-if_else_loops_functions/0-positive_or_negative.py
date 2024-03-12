1b31fcf9ede21d189ba879a070fd385cf19f6ee9
may10-codes <simonm487@gmail.com>
2024-03-12 10:42:31 -0700

    updated code

--- 0x01-python-if_else_loops_functions/0-positive_or_negative.py
+++ 0x01-python-if_else_loops_functions/0-positive_or_negative.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 import random
 number = random.randint(-10, 10)
-number = float(input("Enter a number: "))  # Assuming you're taking input from the user
-
 if number > 0:
-     print(number, "is positive")
+    print(f"{number} is positive")
 elif number == 0:
-    print(number, "is zero")
+    print(f"{number} is zero")
 else:
-    print(number, "is negative")
+    print(f"{number} is negative")

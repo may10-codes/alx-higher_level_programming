14878a95da250eac1848f69ff6f6e564d9ad1638
may10-codes <simonm487@gmail.com>
2024-03-16 10:31:25 -0700

    tasks on data structures

---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+def add_tuple(tuple_a=(), tuple_b=()):
+    element1 = tuple_a + (0, 0)
+    element2 = tuple_b + (0, 0)
+
+    result1 = 0
+    result2 = 0
+
+    result1 += element1[0] + element2[0]
+    result2 += element1[1] + element2[1]
+
+    return (result1, result2)

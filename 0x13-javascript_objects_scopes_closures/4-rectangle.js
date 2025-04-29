4f4e74af30471b8fb4e174c66e597146bc104b53
may10-codes <simonm487@gmail.com>
2025-04-29 17:00:06 +0000

    additional files

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,32 @@
+#!/usr/bin/node
+class Rectangle {
+	  constructor (w, h) {
+		      if (Number.isInteger(w) && w > 0 && Number.isInteger(h) && h > 0) {
+			            this.width = w;
+			            this.height = h;
+			          }
+		    }
+
+	  print () {
+		      let r = '';
+		      for (let i = 0; i < this.width; i++) {
+			            r += 'X';
+			          }
+		      for (let i = 0; i < this.height; i++) {
+			            console.log(r);
+			          }
+		    }
+
+	  rotate () {
+		      const th = this.height;
+		      this.height = this.width;
+		      this.width = th;
+		    }
+
+	  double () {
+		      this.height = 2 * this.height;
+		      this.width = 2 * this.width;
+		    }
+}
+
+module.exports = Rectangle;
